534c03ab89ca2bd632673e68ba7fd91cd3e4b0ce
Lenuta Zbona <Lenuta.Zbona@wkfs-frc.local>
2021-01-06 15:39:17 +0200

    first commit

---
+++ src/main/java/com/example/catalog/domain/Person.java
@@ -0,0 +1,15 @@
+package com.example.catalog.domain;
+
+import org.apache.tomcat.jni.Address;
+
+public abstract class Person {
+
+    private int id;
+    private String fistName;
+    private String lastName;
+    private String phoneNumber;
+    private Address adress;
+    private int age;
+    private String email;
+
+}
